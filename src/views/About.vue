7af7a91f1be9012fbda405f3f6c47d6a3c09833a
lucas-jb <lucasjuarezbarrios@gmail.com>
2023-01-06 20:53:13 +0100

    style about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,19 @@
 <template>
-    About Todo List App Vue
+    <div>
+        <h1>About</h1>
+        <h3>Hey! This is a Todo list App in Vue. Check my github profile: <a href="https://github.com/lucas-jb">lucas-jb</a></h3>
+    </div>
 </template>
+
+<style scoped>
+h1 {
+    color: var(--accent-color);
+}
+h3 {
+    color: var(--text-color);
+}
+a {
+    color: var(--accent-color);
+    text-decoration: none;
+}
+</style>
